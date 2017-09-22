d897c0fba26ce6eeba41dbfc93e2da93f6125bd8
Bharat saraswal <bharat.saraswal@huawei.com>
2017-09-22 19:58:54 +0530

    Resolved below sonar issues.
    removed redundant code.
    changed nested if condition to switch case.
    added method for resuablity of string builder.
    rename variable to follow camelCase.
    removed tab char and changed them with spaces.
    
    Issue-ID:SO-98
    
    Change-Id: If4cf02dede7903ed8b35e4e6879b8691d4f3c48d
    Signed-off-by: Bharat saraswal <bharat.saraswal@huawei.com>

--- keystone-client/src/main/java/com/woorea/openstack/keystone/v3/Keystone.java
+++ keystone-client/src/main/java/com/woorea/openstack/keystone/v3/Keystone.java
@@ -1,73 +1,72 @@
 package com.woorea.openstack.keystone.v3;
 
-import com.woorea.openstack.keystone.v3.api.DomainsResource;
-import com.woorea.openstack.keystone.v3.api.EndpointsResource;
-import com.woorea.openstack.keystone.v3.api.RolesResource;
-import com.woorea.openstack.keystone.v3.api.ServicesResource;
-import com.woorea.openstack.keystone.v3.api.ProjectsResource;
-import com.woorea.openstack.keystone.v3.api.TokensResource;
-import com.woorea.openstack.keystone.v3.api.UsersResource;
 import com.woorea.openstack.base.client.OpenStackClient;
 import com.woorea.openstack.base.client.OpenStackClientConnector;
+import com.woorea.openstack.keystone.v3.api.DomainsResource;
+import com.woorea.openstack.keystone.v3.api.EndpointsResource;
+import com.woorea.openstack.keystone.v3.api.ProjectsResource;
+import com.woorea.openstack.keystone.v3.api.RolesResource;
+import com.woorea.openstack.keystone.v3.api.ServicesResource;
+import com.woorea.openstack.keystone.v3.api.TokensResource;
+import com.woorea.openstack.keystone.v3.api.UsersResource;
 
 public class Keystone extends OpenStackClient {
-	
-	private final TokensResource TOKENS;
-	
-	private final DomainsResource DOMAINS;
-	
-	private final ProjectsResource PROJECTS;
-	
-	private final UsersResource USERS;
-	
-	private final RolesResource ROLES;
-	
-	private final ServicesResource SERVICES;
-	
-	private final EndpointsResource ENDPOINTS;
-	
-	public Keystone(String endpoint, OpenStackClientConnector connector) {
-		super(endpoint, connector);
-		TOKENS = new TokensResource(this);
-		DOMAINS = new DomainsResource(this);
-		PROJECTS = new ProjectsResource(this);
-		USERS = new UsersResource(this);
-		ROLES = new RolesResource(this);
-		SERVICES = new ServicesResource(this);
-		ENDPOINTS = new EndpointsResource(this);
-	}
-	
-	public Keystone(String endpoint) {
-		this(endpoint, null);
-	}
-	
-	public TokensResource tokens() {
-		return TOKENS;
-	}
-	
-	public DomainsResource domains() {
-		return DOMAINS;
-	}
-	
-	public ProjectsResource projects() {
-		return PROJECTS;
-	}
-	
-	public UsersResource users() {
-		return USERS;
-	}
-	
-	public RolesResource roles() {
-		return ROLES;
-	}
-	
-	public ServicesResource services() {
-		return SERVICES;
-	}
-	
-	public EndpointsResource endpoints() {
-		return ENDPOINTS;
-	}
 
+    private final TokensResource tokens;
+
+    private final DomainsResource domains;
+
+    private final ProjectsResource projects;
+
+    private final UsersResource users;
+
+    private final RolesResource roles;
+
+    private final ServicesResource services;
+
+    private final EndpointsResource endpoints;
+
+    public Keystone(String endpoint, OpenStackClientConnector connector) {
+        super(endpoint, connector);
+        tokens = new TokensResource(this);
+        domains = new DomainsResource(this);
+        projects = new ProjectsResource(this);
+        users = new UsersResource(this);
+        roles = new RolesResource(this);
+        services = new ServicesResource(this);
+        endpoints = new EndpointsResource(this);
+    }
+
+    public Keystone(String endpoint) {
+        this(endpoint, null);
+    }
+
+    public TokensResource tokens() {
+        return tokens;
+    }
+
+    public DomainsResource domains() {
+        return domains;
+    }
+
+    public ProjectsResource projects() {
+        return projects;
+    }
+
+    public UsersResource users() {
+        return users;
+    }
+
+    public RolesResource roles() {
+        return roles;
+    }
+
+    public ServicesResource services() {
+        return services;
+    }
+
+    public EndpointsResource endpoints() {
+        return endpoints;
+    }
 }
 
