083f0f9bc39e96b1b8ab0c515e1202b51c3fa04d
seshukm <seshu.kumar.m@huawei.com>
2018-07-25 17:59:44 +0800

    Basic checkstyle fixes
    Issue-ID: SO-729
    
    Change-Id: I9ee8e0b8519ac9110a56b48aa17acaa524d70a30
    Signed-off-by: seshukm <seshu.kumar.m@huawei.com>

--- ceilometer-client/src/main/java/com/woorea/openstack/ceilometer/QueriableCeilometerCommand.java
+++ ceilometer-client/src/main/java/com/woorea/openstack/ceilometer/QueriableCeilometerCommand.java
@@ -23,52 +23,52 @@ import java.util.List;
 import com.woorea.openstack.base.client.OpenStackRequest;
 
 public abstract class QueriableCeilometerCommand<T,R> extends OpenStackRequest<R> {
-	
-	protected List<String> fields = new ArrayList<>();
-	
-	protected List<String> ops = new ArrayList<>();
-	
-	protected List<Serializable> values = new ArrayList<>();
-	
-	private T filter(String field, String op, Serializable value) {
-		fields.add(field);
-		ops.add(op);
-		values.add(value);
-		return (T) this;
-	}
-	
-	public T lt(String field, Serializable value) {
-		return filter(field, "lt", value);
-	}
-	
-	public T le(String field, Serializable value) {
-		return filter(field, "le", value);
-	}
-	
-	public T eq(String field, Serializable value) {
-		return filter(field, "eq", value);
-	}
-	
-	public T ne(String field, Serializable value) {
-		return filter(field, "ne", value);
-	}
-	
-	public T ge(String field, Serializable value) {
-		return filter(field, "ge", value);
-	}
-	
-	public T gt(String field, Serializable value) {
-		return filter(field, "gt", value);
-	}
+    
+    protected List<String> fields = new ArrayList<>();
+    
+    protected List<String> ops = new ArrayList<>();
+    
+    protected List<Serializable> values = new ArrayList<>();
+    
+    private T filter(String field, String op, Serializable value) {
+        fields.add(field);
+        ops.add(op);
+        values.add(value);
+        return (T) this;
+    }
+    
+    public T lt(String field, Serializable value) {
+        return filter(field, "lt", value);
+    }
+    
+    public T le(String field, Serializable value) {
+        return filter(field, "le", value);
+    }
+    
+    public T eq(String field, Serializable value) {
+        return filter(field, "eq", value);
+    }
+    
+    public T ne(String field, Serializable value) {
+        return filter(field, "ne", value);
+    }
+    
+    public T ge(String field, Serializable value) {
+        return filter(field, "ge", value);
+    }
+    
+    public T gt(String field, Serializable value) {
+        return filter(field, "gt", value);
+    }
 
-	/*
-	public WebTarget query(WebTarget target) {
-		if(fields.size() > 0) {
-			target = target.queryParam("q.field", fields.toArray());
-			target = target.queryParam("q.op", ops.toArray());
-			target = target.queryParam("q.value", values.toArray());
-		}
-		return target;
-	}
-	*/
+    /*
+    public WebTarget query(WebTarget target) {
+        if(fields.size() > 0) {
+            target = target.queryParam("q.field", fields.toArray());
+            target = target.queryParam("q.op", ops.toArray());
+            target = target.queryParam("q.value", values.toArray());
+        }
+        return target;
+    }
+    */
 }
