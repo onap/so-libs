d7d05d563996d179e3af89362f5400ab1a1318c9
Isaac Manuel Raj <isaac.manuelraj@huawei.com>
2019-04-12 11:47:43 +0530

    Code Enhancement (Sonar Fixes)
    Sonar Issues Fixes
    
    Issue-ID: SO-1490
    
    Change-Id: I1e1782b6c98b21d339004374c9bd92839f095c5d
    Signed-off-by: Isaac Manuel Raj <isaac.manuelraj@huawei.com>

--- ceilometer-client/src/main/java/com/woorea/openstack/ceilometer/v2/api/MetersResource.java
+++ ceilometer-client/src/main/java/com/woorea/openstack/ceilometer/v2/api/MetersResource.java
@@ -23,45 +23,42 @@ import com.woorea.openstack.ceilometer.QueriableCeilometerCommand;
 import com.woorea.openstack.ceilometer.v2.model.Sample;
 
 public class MetersResource {
-    
+
     private final OpenStackClient CLIENT;
-    
+
     public MetersResource(OpenStackClient client) {
         CLIENT = client;
     }
-    
+
     public List list() {
         return new List();
     }
-    
+
     public Show show() {
         return new Show();
     }
-    
+
     public Statistics statistics() {
         return new Statistics();
     }
-    
+
     public class List extends QueriableCeilometerCommand<List, java.util.List<Sample>> {
         public List() {
-            //return query(target.path("meters")).request(MediaType.APPLICATION_JSON).get(new GenericType<List<Meter>>() {});
+
         }
     }
-    
+
     public class Show extends QueriableCeilometerCommand<Show, java.util.List<Sample>> {
 
         private String name;
-            
+
         public Show name(String name) {
             this.name = name;
             return this;
         }
-        
+
         public Show() {
-//            if(name == null) {
-//                throw new UnsupportedOperationException("meter id is mandatory");
-//            }
-//            return query(target.path("meters").path(name)).request(MediaType.APPLICATION_JSON).get(new GenericType<List<Sample>>() {});
+
         }
 
     }
@@ -69,17 +66,14 @@ public class MetersResource {
     public class Statistics extends QueriableCeilometerCommand<Statistics, java.util.List<Statistics>> {
 
         private String name;
-            
+
         public Statistics name(String name) {
             this.name = name;
             return this;
         }
-        
+
         public Statistics() {
-//            if(name == null) {
-//                throw new UnsupportedOperationException("meter id is mandatory");
-//            }
-//            return query(target.path("meters").path(name).path("statistics")).request(MediaType.APPLICATION_JSON).get(new GenericType<List<Statistics>>(){});
+
         }
 
     }
