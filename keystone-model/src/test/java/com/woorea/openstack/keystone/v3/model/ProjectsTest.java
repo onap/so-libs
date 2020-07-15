a6692dcb7855afd89e62dadeb1ecef513473d509
Boslet, Cory (cb645j) <cb645j@att.com>
2020-07-15 16:37:52 -0500

    Fixed the formatting and updated the UT so have values
    Issue-ID: SO-3071
    Change-Id: Ic8aaa0728a43936cd4c6e1ed590e01ba8f0fb384
    Signed-off-by: Boslet, Cory (cb645j) <cb645j@att.com>

--- keystone-model/src/test/java/com/woorea/openstack/keystone/v3/model/ProjectsTest.java
+++ keystone-model/src/test/java/com/woorea/openstack/keystone/v3/model/ProjectsTest.java
@@ -20,15 +20,15 @@
 
 package com.woorea.openstack.keystone.v3.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class ProjectsTest {
 
@@ -37,7 +37,8 @@ public class ProjectsTest {
     private static final String JSON_FULL = "{" + EOL + "  \"projects\" : [ {" + EOL + "    \"id\" : \"id\"," + EOL
             + "    \"name\" : \"name\"," + EOL + "    \"enabled\" : false," + EOL + "    \"domain_id\" : \"domainid\""
             + EOL + "  }, {" + EOL + "    \"id\" : \"id\"," + EOL + "    \"name\" : \"name\"," + EOL
-            + "    \"enabled\" : false," + EOL + "    \"domain_id\" : \"domainid\"," + EOL + "    \"tags\" : []" + EOL + "  } ]" + EOL + "}";
+            + "    \"enabled\" : false," + EOL + "    \"domain_id\" : \"domainid\"," + EOL
+            + "    \"tags\" : [\"someTag\"]" + EOL + "  } ]" + EOL + "}";
 
     private ObjectMapper objectMapper = new ObjectMapper().setSerializationInclusion(Include.NON_NULL)
             .enable(SerializationFeature.INDENT_OUTPUT).enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
